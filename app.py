146b9db349c5bb2ca5125d87c1fde7a0f48be167
ppeeraya <89898356+ppeeraya@users.noreply.github.com>
2022-01-23 05:22:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -160,9 +160,9 @@ def event_handle(event):
             line_bot_api.reply_message(rtoken, replyObj)
        else :
            headers = request.heasers
-           json_headers = ( {k:v for k,  v  in  headers.items( )} )
-           json_headers.update( { ‘Host’ : ‘bots.dialogflow.com’ } )
-           url :https://dialogflow.cloud.google.com/v1/integrations/line/webhook/ab0619de-ae97-4389-9f2e-56e894a66f6a
+           json_headers = ({k:v for k, v in headers.items()})
+           json_headers.update({ 'Host':'dialogflow.cloud.google.com'})
+           url :"https://dialogflow.cloud.google.com/v1/integrations/line/webhook/ab0619de-ae97-4389-9f2e-56e894a66f6a"
            requests.post(usl,data=json_line, headers=json_headers)
   elif msgType == "image":
       try:
